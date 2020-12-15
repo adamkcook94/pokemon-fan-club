e1af732f9d2abb564c161e1485469a2cd0b793b3
adamkcook94 <adamkcook94@hotmail.com>
2020-12-15 14:55:27 +0000

    created modal with slideshow container inside it

--- javascript/my-script.js
+++ javascript/my-script.js
@@ -12,3 +12,44 @@ $('.navbar a').on('click', function (e) {
         );
     }
 });
+
+// Modal
+
+// modal javascript
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Slideshow
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 6000);
+}
