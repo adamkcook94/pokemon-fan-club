35c2bf680e59dc78209ddd45a0c05789737bb93d
adamkcook94 <adamkcook94@hotmail.com>
2020-12-21 19:32:41 +0000

    created modal for first three games

--- javascript/my-script.js
+++ javascript/my-script.js
@@ -34,8 +34,65 @@ span.onclick = function () {
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    } if (event.target == modal1) {
+        modal1.style.display = "none";
+    } if (event.target == modal2) {
+        modal2.style.display = "none";
     }
 }
+
+
+
+// modal 2
+
+var modal1 = document.getElementById("myModal1");
+
+var btn1 = document.getElementById("myBtn1");
+
+var span = document.getElementsByClassName("close")[1];
+
+btn1.onclick = function () {
+    modal1.style.display = "block";
+}
+
+span.onclick = function () {
+    modal1.style.display = "none";
+}
+
+// modal 3
+
+var modal2 = document.getElementById("myModal2");
+
+var btn2 = document.getElementById("myBtn2");
+
+var span = document.getElementsByClassName("close")[2];
+
+btn2.onclick = function () {
+    modal2.style.display = "block";
+}
+
+span.onclick = function () {
+    modal2.style.display = "none";
+}
+
+// // modal 4 
+
+// var modal3 = document.getElementById("myModal3");
+
+// var btn3 = document.getElementById("myBtn3");
+
+// var span = document.getElementsByClassName("close")[3];
+
+// btn3.onclick = function () {
+//     modal3.style.display = "block";
+// }
+
+// span.onclick = function () {
+//     modal3.style.display = "none";
+// }
+
+
+
 
 // Slideshow
 
@@ -51,8 +108,10 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 6000);
+    setTimeout(showSlides, 1000);
 }
+
+
 
 ScrollReveal().reveal('.headline', { duration: 2000 })
 
