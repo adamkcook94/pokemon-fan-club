7025ffe17123a9f553fb5112964b57ca72189ae0
adamkcook94 <adamkcook94@hotmail.com>
2021-01-03 22:18:02 +0000

    added movie images to swiper js

--- javascript/my-script.js
+++ javascript/my-script.js
@@ -459,13 +459,12 @@ var swiper = new Swiper('.swiper-container', {
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
-        rotate: 50,
+        rotate: 20,
         stretch: 0,
-        depth: 100,
+        depth: 200,
         modifier: 1,
         slideShadows: true,
+
     },
-    pagination: {
-        el: '.swiper-pagination',
-    },
+    loop: true,
 });
