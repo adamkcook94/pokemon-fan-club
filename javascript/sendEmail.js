50619d9f3a614644e94cee9e22b5a257c1dbe69e
adamkcook94 <adamkcook94@hotmail.com>
2021-01-06 01:51:55 +0000

    added pop up alert for invalid email addresses in fan events section

--- javascript/sendEmail.js
+++ javascript/sendEmail.js
@@ -1,3 +1,15 @@
+function ValidateEmail(mail) {
+    console.log(`Validating email ${mail}`);
+    if (
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+            mail
+        )
+    ) {
+        return true;
+    }
+    return false;
+}
+
 function sendMail(params) {
     var tempParams = {
         email_address: document.getElementById("emailAddress").value,
@@ -13,47 +25,27 @@ function sendMail(params) {
         })
 };
 
+function sendMail(params) {
+    var tempParams = {
+        email_address: document.getElementById("emailAddress").value,
+        your_name: document.getElementById("yourName").value,
+        postcode: document.getElementById("postcode").value,
+        house_number: document.getElementById("houseNumber").value,
+        street_name: document.getElementById("streetName").value,
+    };
 
+    if (ValidateEmail(tempParams.email_address)) {
+        console.log("success");
+        emailjs
+            .send("service_sgo6zyq", "template_uyv7wkn", tempParams)
+            .then(function (res) {
+                console.log("success", res.status);
+            });
+    } else {
+        alert("Please enter valid email address");
+        return false;
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// window.onload = function () {
-//     document.getElementById('contact-form').addEventListener('submit', function (event) {
-//         event.preventDefault();
-//         // generate a five digit number for the contact_number variable
-//         this.contact_number.value = Math.random() * 100000 | 0;
-//         // these IDs from the previous steps
-//         emailjs.sendForm('contact_service', 'contact_form', this)
-//             .then(function () {
-//                 console.log('SUCCESS!');
-//             }, function (error) {
-//                 console.log('FAILED...', error);
-//             });
-//     });
-// }
-
-//     from_name = getElementbyId('name'),
-//     from_email = getElementbyId('email'),
-//     my_address =  getElementbyId('postcode'),
-// }
+button = document.getElementById("submit");
+button.onclick = sendMail;
